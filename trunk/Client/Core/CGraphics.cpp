ee24e2f4524a8a9dd6ddbe1a213ffcda07017f2c
jenksta <jenksta@1c8cb11d-60c2-6788-c8e6-bfbe79736b29>
2011-09-13 15:19:07 +0000

    Fixed startup issue caused by a patch in CGame::Patch. Added render state saving and restoration to CGraphics. Fixed a memory leak in CIVPedWeapons.

--- trunk/Client/Core/CGraphics.cpp
+++ trunk/Client/Core/CGraphics.cpp
@@ -47,6 +47,7 @@ struct D3DVERTEX
 CGraphics::CGraphics(IDirect3DDevice9 * pDevice)
 {
 	m_pDevice = pDevice;
+	m_pStateBlock = NULL;
 }
 
 CGraphics::~CGraphics()
@@ -56,20 +57,44 @@ CGraphics::~CGraphics()
 
 void CGraphics::OnLostDevice()
 {
-
+	// If we have a state block release it
+	if(m_pStateBlock)
+	{
+		m_pStateBlock->Release();
+		m_pStateBlock = NULL;
+	}
 }
 
 void CGraphics::OnResetDevice()
 {
+	// If we don't have a state block create one
+	if(!m_pStateBlock)
+		m_pDevice->CreateStateBlock(D3DSBT_ALL, &m_pStateBlock);
+}
 
+void CGraphics::Begin()
+{
+	// If we have a state block capture the current device state
+	if(m_pStateBlock)
+		m_pStateBlock->Capture();
+}
+
+void CGraphics::End()
+{
+	// If we have a state block apply the captured device state
+	if(m_pStateBlock)
+		m_pStateBlock->Apply();
 }
 
 void CGraphics::DrawPixel(float fX, float fY, unsigned long ulColor)
 {
 	D3DVERTEX vertex(fX, fY, 0.0f, 1.0f, ulColor);
+
+	Begin();
 	m_pDevice->SetTexture(0, NULL);
 	m_pDevice->SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
 	m_pDevice->DrawPrimitiveUP(D3DPT_POINTLIST, 1, &vertex, sizeof(D3DVERTEX));
+	End();
 }
 
 void CGraphics::DrawLine(float fStartX, float fStartY, float fEndX, float fEndY, unsigned long ulColor)
@@ -77,9 +102,12 @@ void CGraphics::DrawLine(float fStartX, float fStartY, float fEndX, float fEndY,
 	D3DVERTEX vertex[2];
 	vertex[0] = D3DVERTEX(fStartX, fStartY, 0.0f, 1.0f, ulColor);
 	vertex[1] = D3DVERTEX(fEndX, fEndY, 0.0f, 1.0f, ulColor);
+
+	Begin();
 	m_pDevice->SetTexture(0, NULL);
 	m_pDevice->SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
 	m_pDevice->DrawPrimitiveUP(D3DPT_LINELIST, 1, &vertex[0], sizeof(D3DVERTEX));
+	End();
 }
 
 void CGraphics::DrawRect(float fX, float fY, float fWidth, float fHeight, unsigned long ulColor)
@@ -92,7 +120,9 @@ void CGraphics::DrawRect(float fX, float fY, float fWidth, float fHeight, unsign
 	vertex[3] = D3DVERTEX(fX, (fY + fHeight), 0.0f, 1.0f, ulColor);
 	short indices[6] = {0,1,2,0,2,3};
 
+	Begin();
 	m_pDevice->SetTexture(0, NULL);
 	m_pDevice->SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
 	m_pDevice->DrawIndexedPrimitiveUP(D3DPT_TRIANGLELIST, 0, 4, 2, &indices[0], D3DFMT_INDEX16, &vertex[0], sizeof(D3DVERTEX));
+	End();
 }
