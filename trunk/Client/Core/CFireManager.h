1daea3f9a38c04cb07e01d500908becfe12f4152
ru.mysqld@gmail.com <ru.mysqld@gmail.com@1c8cb11d-60c2-6788-c8e6-bfbe79736b29>
2012-06-13 16:21:22 +0000

    Fix for prev. commit

---
+++ trunk/Client/Core/CFireManager.h
@@ -0,0 +1,34 @@
+//============== IV: Multiplayer - http://code.iv-multiplayer.com ==============
+//
+// File: CObjectManager.h
+// Project: Client.Core
+// Author(s): FRi
+// License: See LICENSE in root directory
+//
+//==============================================================================
+
+#pragma once
+#include <Common.h>
+#define MAX_FIRE 32
+
+struct _Fire
+{
+	unsigned int uiHandle;
+	float fdensity;
+	CVector3 vecPos;
+};
+
+class CFireManager
+{
+private:
+	bool m_bActive[MAX_FIRE];
+	_Fire m_Fire[MAX_FIRE];
+
+public:
+	CFireManager();
+	~CFireManager();
+
+	void Create(EntityId fireId, CVector3& vecPos, float fdensity);
+	void Delete(EntityId fireId);
+	bool DoesExist(EntityId fireId) { return m_bActive[fireId]; };
+};
