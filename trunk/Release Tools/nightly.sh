95256b43421495545ba9b6d9b40af97b2410e588
ru.mysqld@gmail.com <ru.mysqld@gmail.com@1c8cb11d-60c2-6788-c8e6-bfbe79736b29>
2012-06-21 09:17:53 +0000

    Added linux nightly script

---
+++ trunk/Release Tools/nightly.sh
@@ -0,0 +1,6 @@
+cd ../
+make
+cd "Release Tools"
+GZIPFILE=IVMP-r`svnversion`-Linux-Nightly-Server.tar.gz
+tar cfvz $GZIPFILE ../Binary/ivmp-svr ../Binary/Network.Core.so ../Binary/webserver ../Binary/scripts ../Binary/resources ../Binary/modules ../Binary/files ../Binary/clientscripts ../Binary/settings.xml LICENSE --exclude=.svn
+cp $GZIPFILE /www/adamix.iv-m.com/nightly/
