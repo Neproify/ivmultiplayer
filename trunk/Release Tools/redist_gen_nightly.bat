76bf2bf52640b9c3af50c7c38e32ad950c4b3c46
ru.mysqld@gmail.com <ru.mysqld@gmail.com@1c8cb11d-60c2-6788-c8e6-bfbe79736b29>
2012-06-23 09:21:00 +0000

    Ok, now let the nightly scripts use our private 'Release' network module.

--- trunk/Release Tools/redist_gen_nightly.bat
+++ trunk/Release Tools/redist_gen_nightly.bat
@@ -4,7 +4,11 @@ for /f %%i in ('svnversion') do set VERSION=r%%i-Win32-Nightly
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 cd ..
 devenv.exe IVMP.sln /build Release
+cd Network.Release
+call build.bat
+cd ..
 cd "Release Tools"
+
 
 7z a IVMP-%VERSION%-Server.zip ../Binary/ivmp-svr.exe
 7z a IVMP-%VERSION%-Server.zip ../Binary/Network.Core.dll
