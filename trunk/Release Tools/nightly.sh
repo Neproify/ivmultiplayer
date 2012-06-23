97d5fbabeb1e6ded9e73eb42f4ed2c2f8fc45d10
ru.mysqld@gmail.com <ru.mysqld@gmail.com@1c8cb11d-60c2-6788-c8e6-bfbe79736b29>
2012-06-23 09:35:35 +0000

    Let the linux nightly use our private net module.

--- trunk/Release Tools/nightly.sh
+++ trunk/Release Tools/nightly.sh
@@ -1,6 +1,9 @@
 cd ../
 svn up
 make
+cd Network.Release/Core
+make
+cd ../..
 cd "Release Tools"
 GZIPFILE=IVMP-r`svnversion`-Linux-Nightly-Server.tar.gz
 tar cfvz $GZIPFILE ../Binary/ivmp-svr ../Binary/Network.Core.so ../Binary/webserver ../Binary/scripts ../Binary/resources ../Binary/modules ../Binary/files ../Binary/clientscripts ../Binary/settings.xml LICENSE --exclude=.svn
