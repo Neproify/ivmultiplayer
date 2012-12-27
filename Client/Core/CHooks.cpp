d3d9d28004b650ceded06f1641240c950d01214c
FRi.developing <FRi.developing@1c8cb11d-60c2-6788-c8e6-bfbe79736b29>
2012-12-27 21:05:09 +0000

    -Fixed player StreamIn(bad bad XForce.. :P) -Added new crash-prevention-hooks

--- Client/Core/CHooks.cpp
+++ Client/Core/CHooks.cpp
@@ -153,6 +153,11 @@ void CHooks::Install()
 	CPatcher::InstallJmpPatch((GetBase() + 0x447270), (DWORD)PoolCalculationHook);
 	CPatcher::InstallJmpPatch((GetBase() + 0x7B79E0), (DWORD)CrackedStartup);
 	
+	// Prevent crashes on player connect(associated with ped intelligence)
+	CPatcher::InstallJmpPatch((GetBase() + 0x815380), (GetBase() + 0x8153D4));
+	CPatcher::InstallJmpPatch((GetBase() + 0x625F15), (GetBase() + 0x625F1D));
+
+	
 	//CPatcher::InstallJmpPatch((GetBase() + 0x9E2DC0), (GetBase() + 0x9E2E24)); // camera stuff
 	//CPatcher::InstallJmpPatch((GetBase() + 0x8A7890), (GetBase() + 0x8A7D64)); // unknown
 	//CPatcher::InstallJmpPatch((GetBase() + 0x8A28B0), (GetBase() + 0x8A297A));
